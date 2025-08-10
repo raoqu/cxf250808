4c2186a3058c94b414a94175d4b57240dbdbe6cc
raoqu <raoqu@msn.com>
2025-08-10 18:39:50 +0800

    update deploy script.

--- publish.sh
+++ publish.sh
@@ -1,2 +1,12 @@
 #!/bin/sh
-rsync -avz ./ root@10.6.0.1:/www/wwwroot/hbjs/
+
+# Use rsync to copy files while excluding .git folder and other unnecessary files
+rsync -avz --progress \
+  --exclude=".git/" \
+  --exclude=".gitignore" \
+  --exclude=".DS_Store" \
+  --exclude="Caddyfile" \
+  --exclude="publish.sh" \
+  ./ root@10.6.0.1:/www/wwwroot/hbjs/
+
+echo "Deployment complete!"
